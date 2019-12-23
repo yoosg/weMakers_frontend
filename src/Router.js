2dadcb716107a660c715687478ee0d3d714b4dc3
yoosg <yousk6347@gmail.com>
2019-12-23 10:31:00 +0900

    Initial Setting

---
+++ src/Router.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import App from "./App";
+/* import Signup from './Pages/Signup'; */
+
+function routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={App} />
+        {/* <Route exact path="/signup" component={Signup} /> */}
+      </Switch>
+    </Router>
+  );
+}
+export default routes;
