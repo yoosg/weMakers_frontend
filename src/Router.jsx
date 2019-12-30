306dbedc21583ab96bb4e1b017bc6117639b06f6
dahyeyudev <dahye.yu.dev@gmail.com>
2019-12-30 11:48:24 +0900

    SignUp page without fetch, modified Router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
-import '../src/common/reset.scss'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import '../src/common/reset.scss';
 import SignUp from './components/signUp/SignUp';
 
 function routes() {
@@ -9,8 +9,6 @@ function routes() {
       <Switch>
         {/* <Route exact path="/" component={App} /> */}
         <Route path="/signup" component={SignUp} />
-        {/* <Route path="/like" component={MyPage} /> */}
-        {/* <Route path="/order" component={MyPage} /> */}
       </Switch>
     </Router>
   );
