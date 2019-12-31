2a6668a2eebbc09b3647d8e85918c6eabfb89d22
dahyeyudev <dahye.yu.dev@gmail.com>
2019-12-31 16:33:35 +0900

    completed login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -60,7 +60,7 @@ class Login extends Component {
   //서버 api
   sendLoginData = () => {
     console.log('fetch지금되고있음');
-    fetch('http://10.58.1.149:8000/user/auth', {
+    fetch('http://10.58.1.149:8001/user/auth', {
       method: 'POST',
       body: JSON.stringify({
         email: this.state.email,
