01fdf90cbe1ebced832040b0ff7e32fdc3cf6b97
South <kimhyoungnams@gmail.com>
2019-12-31 21:30:08 +0900

    fix productDetail big character

--- src/components/ProductDetail/ProductReview/ProductReview.jsx
+++ src/components/ProductDetail/ProductReview/ProductReview.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ProductReviewElement from "./ProductReviewElement";
-import fetchAPI from "../../../Utils/fetch";
+import fetchAPI from "../../../utils/fetch";
 import sty from "./ProductReview.module.scss";
 
 export default class ProductReview extends Component {
@@ -128,7 +128,7 @@ export default class ProductReview extends Component {
             </div>
           </div>
         </div>
-        <a href='https://makers.kakao.com/my/review?menu=wait'>
+        <a href="https://makers.kakao.com/my/review?menu=wait">
           <span className={sty.writeLink}>
             구매후기는 <span className={sty.point}>후기내역</span>에서 작성하실
             수 있습니다.
