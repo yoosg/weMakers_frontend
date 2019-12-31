b772ed85ca59626a733207a6ab7f6e26af7e2ac5
dahyeyudev <dahye.yu.dev@gmail.com>
2019-12-31 18:38:28 +0900

    resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+import "./common/reset.scss";
+import sty from "./App.module.scss";
+import Header from "./components/Header/Header";
+import SideDrawer from "./pages/SideDrawer";
 
-
-function App() {
-  return <div className="kakaoWrap">Hello, Wemakers!!!</div>;
+export default class App extends Component {
+  state = {
+    sideMenu: false
+  };
+  switchToggle = () => {
+    this.setState(prevState => {
+      return { sideMenu: !prevState.sideMenu };
+    });
+  };
+  render() {
+    const { sideMenu } = this.state;
+    return (
+      <div className={sty.app}>
+        <SideDrawer show={sideMenu} switchToggle={this.switchToggle} />
+        <div>
+          <Header switchToggle={this.switchToggle} />
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
