3162f37ba5aedd07294a9452faf30ddba6ab344d
South <kimhyoungnams@gmail.com>
2020-01-02 18:15:14 +0900

    set dynamic route in home

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -1,51 +1,45 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import sty from "./NavBar.module.scss";
 import Tab from "../../atoms/Tab";
-export default class NavBar extends Component {
-  state = {
-    tabs: [
-      { tabName: "홈", active: true },
-      { tabName: "아티스트", active: false },
-      { tabName: "신규", active: false },
-      { tabName: "오픈예정", active: false },
-      { tabName: "마이", active: false }
-    ]
-  };
+export default withRouter(
+  class NavBar extends Component {
+    state = {
+      tabs: [
+        { tabName: "홈", link: "", active: false },
+        { tabName: "아티스트", link: "artist", active: false },
+        { tabName: "신규", link: "new", active: false },
+        { tabName: "오픈예정", link: "open", active: false },
+        { tabName: "마이", link: "my", active: false }
+      ]
+    };
+    componentDidMount() {
+      const newState = this.state.tabs.map(x => {
+        if (x.link === this.props.match.url.substr(1))
+          return { ...x, active: true };
+        return { ...x };
+      });
+      this.setState({ tabs: [...newState] });
+    }
+    render() {
+      const { tabs } = this.state;
+      const tabList = tabs.map(item => {
+        return (
+          <Tab
+            key={item.tabName}
+            tabName={item.tabName}
+            link={item.link}
+            tabLen={tabs.length}
+            active={item.active}
+          />
+        );
+      });
 
-  //change tab focus
-  changeTabActive = target => () => {
-    const { tabs } = this.state;
-    const preActiveIndex = tabs.findIndex(x => x.active === true);
-    const newActiveState = tabs.map((x, idx) => {
-      if (idx === preActiveIndex && x.tabName !== target)
-        return { ...x, active: false };
-      if (x.tabName === target) return { ...x, active: true };
-      return x;
-    });
-    //set new tabs state
-    this.setState({
-      tabs: [...newActiveState]
-    });
-  };
-
-  render() {
-    const { tabs } = this.state;
-    const tabList = tabs.map(item => {
       return (
-        <Tab
-          key={item.tabName}
-          tabName={item.tabName}
-          tabLen={tabs.length}
-          active={item.active}
-          changeTabActive={this.changeTabActive}
-        />
+        <>
+          <ul className={sty.ulWrapper}>{tabList}</ul>
+        </>
       );
-    });
-
-    return (
-      <>
-        <ul className={sty.ulWrapper}>{tabList}</ul>
-      </>
-    );
+    }
   }
-}
+);
---
+++ src/pages/Test1/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Layout from "../../Layout";
+export default function Test1() {
+  return (
+    <Layout>
+      <div>test1</div>;
+    </Layout>
+  );
+}
